29499b1b4f77ea27ae659e91d8968f06e2d97692
tomas-barros1 <tomasabbarros3@gmail.com>
2024-02-26 10:44:22 -0300

    todo list created

---
+++ script.js
@@ -0,0 +1,29 @@
+let addBtn = document.getElementById("addBtn")
+let list = document.getElementById("list")
+
+addBtn.addEventListener("click", function() {
+  let text = document.getElementById("text").value
+
+  if (text !== "") {
+    let divTask = document.createElement("div")
+    let task = document.createElement("p")
+    let buttonRemove = document.createElement("button")
+
+    buttonRemove.classList.add("removeBtn")
+    divTask.classList.add("divTask")
+    buttonRemove.innerHTML = "REMOVER"
+
+    task.innerHTML = text
+  
+    divTask.appendChild(task)
+    divTask.appendChild(buttonRemove)
+    list.appendChild(divTask)
+
+    buttonRemove.addEventListener("click", function() {
+      if (confirm("Are you sure you want to delete the task?"))
+        list.removeChild(divTask)
+    })
+  } else {
+    alert("Fill the camp!")
+  }
+})
